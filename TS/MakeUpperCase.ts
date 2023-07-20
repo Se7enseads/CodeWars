beae0b52a2acdaf8db8bafa3835eac053a700420
Kyle Mututo <135003824+Se7enseads@users.noreply.github.com>
2023-07-20 17:46:35 +0300

    TS: Completed "MakeUpperCase " kata.

---
+++ TS/MakeUpperCase.ts
@@ -0,0 +1,15 @@
+/* 
+
+DESCRIPTION:
+
+Write a function which converts the input string to uppercase.
+
+Kata Link:
+
+https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7
+
+*/
+
+export function makeUpperCase(str: string): string {
+  return str.toUpperCase();
+}
